1878566bcaefdf9137087eb6579645122ba586dd
Shivaram008 <ram.cheruku@cognine.com>
2021-07-30 14:35:58 +0530

    BookStore

---
+++ BookStore/Controllers/BookController.cs
@@ -0,0 +1,28 @@
+﻿using BookStore.Models;
+using BookStore.Models.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository=null;
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+        public ViewResult GetAllBooks()
+        {
+            var data = _bookRepository.GetAllBooks();
+            return View(); 
+        }
+        public List<BookModel> GetBook(int id)
+        {
+            return _bookRepository.GetBook(id);
+        }
+    }
+}
---
+++ BookStore/Models/Repository/BookRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Models.Repository
+{
+    public class BookRepository 
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+
+        public List<BookModel> GetBook(int id)
+        {
+            return DataSource().Where(x => x.id == id).ToList();
+        }
+        public List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){id=1,Title="C#",Author="Ram" },
+                new BookModel(){id=2,Title="F#",Author="Siva" },
+                new BookModel(){id=3,Title="ASP",Author="Ram" },
+                new BookModel(){id=4,Title="C#",Author="Prasad" }
+            };
+        }
+    }
+}
